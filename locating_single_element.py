effeff4591f15621c0abbb3c6b1beba1849cb51c
saketbhatnagar1 <165547660+saketbhatnagar1@users.noreply.github.com>
2024-12-11 18:22:12 +0530

    Update locating_single_element.py

--- locating_single_element.py
+++ locating_single_element.py
@@ -3,10 +3,10 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 driver = webdriver.Chrome() 
-query = 'laptop'
-driver.get(f"https://www.amazon.in/s?k={query}&crid=293ZV7B0Q6PQC&sprefix=laptop%2Caps%2C208&ref=nb_sb_noss_2") #Go to python.org
+query = ''#Optional
+driver.get(f"URL") #Go to python.org
 
-elem = driver.find_element(By.CLASS_NAME,"puis-card-container")
+elem = driver.find_element(By.CLASS_NAME,"ClassName")
 print(elem.get_attribute("outerHTML"))
 time.sleep(20)
 driver.close()
